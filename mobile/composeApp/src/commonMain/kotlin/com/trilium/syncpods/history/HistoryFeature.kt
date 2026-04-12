9ab80df5d927cbc2247da32a9af25b182ab2b1a8
Reid McPherson <reid.mcpherson@gmail.com>
2026-04-11 22:00:18 -0700

    fix: declare _effects before init block in HistoryFeature

--- mobile/composeApp/src/commonMain/kotlin/com/trilium/syncpods/history/HistoryFeature.kt
+++ mobile/composeApp/src/commonMain/kotlin/com/trilium/syncpods/history/HistoryFeature.kt
@@ -78,6 +78,9 @@ class HistoryFeature(
     progressUpdates: Flow<Unit> = emptyFlow(),
 ) : StandardFeature<HistoryState, HistoryEvent, HistoryAction, HistoryResult, HistoryEffect>(scope) {
 
+    private val _effects = MutableSharedFlow<HistoryEffect>(extraBufferCapacity = 8)
+    override val effects: SharedFlow<HistoryEffect> get() = _effects
+
     init {
         scope.launch {
             progressUpdates.collect { process(HistoryEvent.ProgressSaved) }
