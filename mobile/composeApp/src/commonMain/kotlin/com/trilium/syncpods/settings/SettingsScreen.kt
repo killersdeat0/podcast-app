b93a90822df56a1b7f10408ef057aebc85379934
Reid McPherson <reid.mcpherson@gmail.com>
2026-05-20 22:40:23 -0700

    fix: sort LocalUriHandler import into correct androidx.compose.ui group

--- mobile/composeApp/src/commonMain/kotlin/com/trilium/syncpods/settings/SettingsScreen.kt
+++ mobile/composeApp/src/commonMain/kotlin/com/trilium/syncpods/settings/SettingsScreen.kt
@@ -34,7 +34,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
-import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -45,6 +44,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.unit.dp
 import com.trilium.syncpods.isDebug
 import kotlinx.coroutines.delay
